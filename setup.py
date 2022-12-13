83e2d201fd5c38c7ea9f68770228148436026a2d
Wietze Helmantel <helmantel@pythonunited.com>
2022-12-13 10:40:07 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="djpwr",
+      version="1.0.0",
+      description="Django for Power Users",
+      license='MIT',
+      author="Ruud de Klerk",
+      author_email="ruud@codeveloped.nl",
+      url='https://github.com/djpwr/djpwr',
+      packages=['djpwr']
+)
