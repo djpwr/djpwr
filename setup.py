e57b465e3d2192c4d7b2613059296cba48e1b7a3
Ruud de Klerk <ruud@codeveloped.nl>
2022-12-14 09:38:37 +0100

    Set version to 0.0.1

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name="djpwr",
-      version="1.0.0",
-      description="Django for Power Users",
-      license='MIT',
-      author="Ruud de Klerk",
-      author_email="ruud@codeveloped.nl",
-      url='https://github.com/djpwr/djpwr',
-      packages=find_packages()
+setup(
+    name="djpwr",
+    version="0.0.1",
+    description="Django for Power Users",
+    license='MIT',
+    author="Ruud de Klerk",
+    author_email="ruud@codeveloped.nl",
+    url='https://github.com/djpwr/djpwr',
+    packages=find_packages()
 )
